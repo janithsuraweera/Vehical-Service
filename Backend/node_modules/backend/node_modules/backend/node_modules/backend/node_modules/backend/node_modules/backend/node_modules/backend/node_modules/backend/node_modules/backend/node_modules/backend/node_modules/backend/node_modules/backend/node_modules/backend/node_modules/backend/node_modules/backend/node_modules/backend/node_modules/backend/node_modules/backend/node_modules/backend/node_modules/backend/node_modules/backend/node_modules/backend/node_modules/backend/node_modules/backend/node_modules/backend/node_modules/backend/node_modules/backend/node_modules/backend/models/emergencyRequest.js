b4326113f005881a2073b2b0c93f89a48129ae9a
Janith Suraweera <153274728+janithsuraweera@users.noreply.github.com>
2025-04-06 21:46:14 +0530

    all

---
+++ Backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/models/emergencyRequest.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+
+const emergencyRequestSchema = new mongoose.Schema({
+    emergencyRequestNo: {
+        type: String,
+        unique: true,
+        default: uuidv4,
+        immutable: false,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    contactNumber: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        },
+        address: {
+            type: String,
+        },
+    },
+    vehicleType: {
+        type: String,
+        enum: ['car', 'motorcycle', 'bus', 'truck', 'other', 'van'],
+        required: true,
+    },
+    vehicleColor: {
+        type: String,
+        required: true,
+    },
+    emergencyType: {
+        type: String,
+        enum: ['breakdown', 'accident', 'flat_tire', 'other'],
+        required: true,
+    },
+    description: String,
+    status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'completed'],
+        default: 'pending',
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    vehicleNumber: {
+        type: String,
+        sparse: true, // Optional field 
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    time: {
+        type: String,
+        default: () => {
+            const now = new Date();
+            return now.toLocaleTimeString(); 
+        },
+    },
+}, { timestamps: false }); // timestamps option is set to false to disable createdAt and updatedAt fields
+
+module.exports = mongoose.model('EmergencyRequest', emergencyRequestSchema);
