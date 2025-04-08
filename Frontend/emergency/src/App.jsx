29fc242e4b73508308e06d2d066ed3fecfa654c4
Shehara0 <sheharasiriwardana21@gmail.com>
2025-04-08 19:40:49 +0530

    home page update and app.jsx path set

--- Frontend/emergency/src/App.jsx
+++ Frontend/emergency/src/App.jsx
@@ -3,27 +3,28 @@ import { ToastContainer } from 'react-toastify';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 
-import EmergencyForm from './components/emergency/EmergencyForm'
-import EmergencyList from './components/emergency/EmergencyList'
-import EmergencyDetails from './components/emergency/EmergencyDetails';
-import UpdateEmergencyForm from './components/emergency/UpdateEmergencyForm';
-import HomePage from './components/emergency/HomePage';
-import LoginForm from './components/emergency/LoginForm';
-import SignupForm from './components/emergency/SignupForm';
-import Dashboard from './components/emergency/Dashboard';
+// import EmergencyForm from './components/emergency/EmergencyForm'
+// import EmergencyList from './components/emergency/EmergencyList'
+// import EmergencyDetails from './components/emergency/EmergencyDetails';
+// import UpdateEmergencyForm from './components/emergency/UpdateEmergencyForm';
+// import LoginForm from './components/emergency/LoginForm';
+// import SignupForm from './components/emergency/SignupForm';
+// import Dashboard from './components/emergency/Dashboard';
 
+
+import HomePage from '././components/inventory/HomePage';
 function App() {
   return (
     <Router>
       <ToastContainer />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/emergency-form" element={<EmergencyForm />} />
+        <Route path="/inventory-form" element={<EmergencyForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/list" element={<EmergencyList />} />
-        <Route path="/emergency/:id" element={<EmergencyDetails />} />
-        <Route path="/update-emergency/:id" element={<UpdateEmergencyForm />} />
+        <Route path="/inventory/:id" element={<EmergencyDetails />} />
+        <Route path="/update-inventory/:id" element={<UpdateEmergencyForm />} />
         <Route path="/dash" element={<Dashboard />} />
       </Routes>
     </Router>
