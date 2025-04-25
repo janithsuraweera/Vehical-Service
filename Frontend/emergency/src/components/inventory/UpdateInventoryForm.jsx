171c43db70e05754ffd0b66540b0ff5880639f8a
Shehara0 <sheharasiriwardana21@gmail.com>
2025-04-26 00:55:34 +0530

    fix error and modify css

--- Frontend/emergency/src/components/inventory/UpdateInventoryForm.jsx
+++ Frontend/emergency/src/components/inventory/UpdateInventoryForm.jsx
@@ -185,7 +185,9 @@ const UpdateInventoryForm = () => {
                     </button>
 
                     <button
-                       
+                        type="button"
+                        onClick={handleBack}
+                        className="bg-gray-600 hover:bg-gray-600 text-white p-3 rounded-lg w-full mt-4 transition-colors duration-300 flex items-center justify-center"
                     >
                         <FaArrowLeft className="mr-2" /> Back
                     </button>
