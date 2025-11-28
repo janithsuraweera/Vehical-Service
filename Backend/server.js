1104da459b43ead9469057c849abe30e74e931f2
Janith Suraweera <153274728+janithsuraweera@users.noreply.github.com>
2025-11-28 10:58:39 +0530

    Remove mongoose dependency from server.js to streamline backend configuration

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');//authorizes the server to accept JSON data in a POST request
